8460cd6de484ee31b89cc3b733ef27cd91c8fb18
Vittal Pai <vittalpai@in.ibm.com>
2021-07-07 16:05:41 +0530

    closures

--- 03-functions/04-demo.go
+++ 03-functions/04-demo.go
@@ -5,6 +5,8 @@ import "fmt"
 func main() {
 	logger(add, 100, 200)
 	logger(subtract, 500, 200)
+	loggerAdd := getloggerfun(add, "Add")
+	fmt.Println(loggerAdd(10, 15))
 }
 
 func add(x, y int) int {
@@ -19,4 +21,14 @@ func logger(fn func(int, int) int, x, y int) {
 	fmt.Println("Begin invocation")
 	fmt.Println(fn(x, y))
 	fmt.Println("After invocation")
+}
+
+func getloggerfun(fn func(int, int) int, operName string) func(int, int) int {
+	return func(x, y int) int {
+		fmt.Println("Begin invocation")
+		result := fn(x, y)
+		fmt.Println("Result =", result)
+		fmt.Println("After invocation")
+		return result
+	}
 }
